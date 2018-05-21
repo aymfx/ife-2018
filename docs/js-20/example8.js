ec76e74ff2e978905917aaba45d1dcf6c4539bdc
aymfx <1095731371@qq.com>
2018-05-21 10:22:11 +0800

    22-24天作业提交

--- docs/js-20/example8.js
+++ docs/js-20/example8.js
@@ -15,3 +15,19 @@ var scoreObject = {
         "Music": 88
     }
 }
+
+let arr = [];
+for (const key in scoreObject) {
+    if (scoreObject.hasOwnProperty(key)) {
+        const element = scoreObject[key];
+        let subarr=[];
+        subarr.push(key)
+        for(const iterator of Object.values(element)) {
+            subarr.push(iterator)
+        }
+        arr.push(subarr);
+        
+    }
+}
+
+console.log(arr)
